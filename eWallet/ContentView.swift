074c4bf694074dd76697b5b0eb5ac004192d7328
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-02 16:19:00 +0200

    Initial Commit

---
+++ eWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eWallet
+//
+//  Created by ramil on 02.03.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
